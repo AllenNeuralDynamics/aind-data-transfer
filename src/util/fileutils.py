26148dd038e4cadf9d36f034e1cd0e028613af9d
Cameron Arshadi <arshadic23@gmail.com>
2022-07-28 17:14:43 -0700

    fix(gcs-upload): fix cloud paths on Windows

--- src/util/fileutils.py
+++ src/util/fileutils.py
@@ -1,5 +1,5 @@
 import os
-from pathlib import PurePath
+from pathlib import PurePath, PurePosixPath
 from typing import List
 
 
@@ -20,17 +20,27 @@ def collect_filepaths(folder: str, recursive: bool = True) -> List[str]:
     return filepaths
 
 
+def join_cloud_paths(cloud_dest_path, relpath):
+    """Always produce posix-style paths, even if relpath
+    is Windows-style"""
+    cloud_dest_path = PurePosixPath(cloud_dest_path)
+    relpath = PurePath(relpath)
+    return str(cloud_dest_path / relpath)
+
+
 def make_cloud_paths(
     filepaths: List[str], cloud_dest_path: str, root: str = None
 ) -> List[str]:
     cloud_paths = []
+    # remove both leading and trailing '/'
+    cloud_dest_path = cloud_dest_path.strip('/')
     for fpath in filepaths:
         if root is None:
             cloud_paths.append(
-                os.path.join(cloud_dest_path, PurePath(fpath).name)
+                join_cloud_paths(cloud_dest_path, PurePath(fpath).name)
             )
         else:
             cloud_paths.append(
-                os.path.join(cloud_dest_path, os.path.relpath(fpath, root))
+                join_cloud_paths(cloud_dest_path, os.path.relpath(fpath, root))
             )
     return cloud_paths
