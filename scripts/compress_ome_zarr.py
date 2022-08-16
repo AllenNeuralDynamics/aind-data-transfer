fae29477b97b337ea2fe3f93e3d15b6067d6fe48
Cameron Arshadi <arshadic23@gmail.com>
2022-08-16 13:23:24 -0700

    refactor(ome-zarr): gather array utils into module

--- scripts/compress_ome_zarr.py
+++ scripts/compress_ome_zarr.py
@@ -1,24 +1,22 @@
 import argparse
 import logging
-import math
+import numpy as np
 import os
 import time
-from pathlib import Path
-
-import numpy as np
-import h5py
 # Importing this alone doesn't work on HPC
 # Must manually override HDF5_PLUGIN_PATH environment variable
 # in each Dask worker
-import hdf5plugin
 from aicsimageio.writers import OmeZarrWriter
 from bids import BIDSLayout
-from cluster.config import load_jobqueue_config
 from dask_jobqueue import SLURMCluster
 from distributed import Client, LocalCluster
 from numcodecs import blosc
-
+from pathlib import Path
 from transfer.transcode.io import DataReaderFactory
+from transfer.util.arrayutils import (pad_array_5d, pad_shape_5d,
+                                      guess_chunks, expand_chunks)
+
+from cluster.config import load_jobqueue_config
 
 blosc.use_threads = False
 
@@ -85,121 +83,6 @@ def get_client(deployment="slurm", **kwargs):
 
     client = Client(cluster)
     return client, config
-
-
-def pad_array_5d(arr):
-    while arr.ndim < 5:
-        arr = arr[np.newaxis, ...]
-    return arr
-
-
-def pad_shape_5d(shape):
-    while len(shape) < 5:
-        shape = (1, *shape)
-    return shape
-
-
-def guess_chunks(data_shape, target_size, bytes_per_pixel, mode="z"):
-    if mode == "z":
-        plane_size = data_shape[3] * data_shape[4] * bytes_per_pixel
-        nplanes_per_chunk = int(math.ceil(target_size / plane_size))
-        nplanes_per_chunk = min(nplanes_per_chunk, data_shape[2])
-        chunks = (
-            1,
-            1,
-            nplanes_per_chunk,
-            data_shape[3],
-            data_shape[4],
-        )
-    elif mode == "cycle":
-        # get the spatial dimensions only
-        spatial_dims = np.array(data_shape)[2:]
-        idx = 0
-        ndims = len(spatial_dims)
-        while np.product(spatial_dims) * bytes_per_pixel > target_size:
-            spatial_dims[idx % ndims] = int(
-                math.ceil(
-                    spatial_dims[idx % ndims] / 2.0
-                )
-            )
-            idx += 1
-        chunks = (
-            1,
-            1,
-            spatial_dims[0],
-            spatial_dims[1],
-            spatial_dims[2]
-        )
-    elif mode == "iso":
-        # TODO: should this be a power of 2?
-        chunk_dim = int(math.ceil((target_size / bytes_per_pixel) ** (1.0 / 3)))
-        chunks = (
-            1,
-            1,
-            min(data_shape[2], chunk_dim),
-            min(data_shape[3], chunk_dim),
-            min(data_shape[4], chunk_dim)
-        )
-    else:
-        raise ValueError(f"Invalid mode {mode}")
-
-    # convert numpy int64 to Python int or zarr will complain
-    return tuple(int(d) for d in chunks)
-
-
-def expand_chunks(chunks, data_shape, target_size, itemsize, mode="iso"):
-    if mode == "cycle":
-        # get the spatial dimensions only
-        spatial_chunks = np.array(chunks)[2:]
-        current = spatial_chunks
-        prev = current.copy()
-        idx = 0
-        ndims = len(spatial_chunks)
-        while _get_size(current, itemsize) < target_size:
-            prev = current.copy()
-            current[idx % ndims] *= 2
-            idx += 1
-        current = _closer_to_target(current, prev, target_size, itemsize)
-        expanded = (
-            1,
-            1,
-            min(data_shape[2], current[0]),
-            min(data_shape[3], current[1]),
-            min(data_shape[4], current[2])
-        )
-    elif mode == "iso":
-        spatial_chunks = np.array(chunks)[2:]
-        current = spatial_chunks
-        prev = current
-        i = 2
-        while _get_size(current, itemsize) < target_size:
-            prev = current
-            current = spatial_chunks * i
-            i += 1
-        current = _closer_to_target(current, prev, target_size, itemsize)
-        expanded = (
-            1,
-            1,
-            min(data_shape[2], current[0]),
-            min(data_shape[3], current[1]),
-            min(data_shape[4], current[2])
-        )
-    else:
-        raise ValueError(f"Invalid mode {mode}")
-
-    return tuple(int(d) for d in expanded)
-
-
-def _closer_to_target(shape1, shape2, target_bytes, itemsize):
-    size1 = _get_size(shape1, itemsize)
-    size2 = _get_size(shape2, itemsize)
-    if abs(size1 - target_bytes) < abs(size2 - target_bytes):
-        return shape1
-    return shape2
-
-
-def _get_size(shape, itemsize):
-    return np.product(shape) * itemsize
 
 
 def validate_output_path(output):
---
+++ src/transfer/util/arrayutils.py
@@ -0,0 +1,117 @@
+import math
+import numpy as np
+
+
+def pad_array_5d(arr):
+    while arr.ndim < 5:
+        arr = arr[np.newaxis, ...]
+    return arr
+
+
+def pad_shape_5d(shape):
+    while len(shape) < 5:
+        shape = (1, *shape)
+    return shape
+
+
+def guess_chunks(data_shape, target_size, bytes_per_pixel, mode="z"):
+    if mode == "z":
+        plane_size = data_shape[3] * data_shape[4] * bytes_per_pixel
+        nplanes_per_chunk = int(math.ceil(target_size / plane_size))
+        nplanes_per_chunk = min(nplanes_per_chunk, data_shape[2])
+        chunks = (
+            1,
+            1,
+            nplanes_per_chunk,
+            data_shape[3],
+            data_shape[4],
+        )
+    elif mode == "cycle":
+        # get the spatial dimensions only
+        spatial_dims = np.array(data_shape)[2:]
+        idx = 0
+        ndims = len(spatial_dims)
+        while np.product(spatial_dims) * bytes_per_pixel > target_size:
+            spatial_dims[idx % ndims] = int(
+                math.ceil(
+                    spatial_dims[idx % ndims] / 2.0
+                )
+            )
+            idx += 1
+        chunks = (
+            1,
+            1,
+            spatial_dims[0],
+            spatial_dims[1],
+            spatial_dims[2]
+        )
+    elif mode == "iso":
+        # TODO: should this be a power of 2?
+        chunk_dim = int(math.ceil((target_size / bytes_per_pixel) ** (1.0 / 3)))
+        chunks = (
+            1,
+            1,
+            min(data_shape[2], chunk_dim),
+            min(data_shape[3], chunk_dim),
+            min(data_shape[4], chunk_dim)
+        )
+    else:
+        raise ValueError(f"Invalid mode {mode}")
+
+    # convert numpy int64 to Python int or zarr will complain
+    return tuple(int(d) for d in chunks)
+
+
+def expand_chunks(chunks, data_shape, target_size, itemsize, mode="iso"):
+    if mode == "cycle":
+        # get the spatial dimensions only
+        spatial_chunks = np.array(chunks)[2:]
+        current = spatial_chunks
+        prev = current.copy()
+        idx = 0
+        ndims = len(spatial_chunks)
+        while _get_size(current, itemsize) < target_size:
+            prev = current.copy()
+            current[idx % ndims] *= 2
+            idx += 1
+        current = _closer_to_target(current, prev, target_size, itemsize)
+        expanded = (
+            1,
+            1,
+            min(data_shape[2], current[0]),
+            min(data_shape[3], current[1]),
+            min(data_shape[4], current[2])
+        )
+    elif mode == "iso":
+        spatial_chunks = np.array(chunks)[2:]
+        current = spatial_chunks
+        prev = current
+        i = 2
+        while _get_size(current, itemsize) < target_size:
+            prev = current
+            current = spatial_chunks * i
+            i += 1
+        current = _closer_to_target(current, prev, target_size, itemsize)
+        expanded = (
+            1,
+            1,
+            min(data_shape[2], current[0]),
+            min(data_shape[3], current[1]),
+            min(data_shape[4], current[2])
+        )
+    else:
+        raise ValueError(f"Invalid mode {mode}")
+
+    return tuple(int(d) for d in expanded)
+
+
+def _closer_to_target(shape1, shape2, target_bytes, itemsize):
+    size1 = _get_size(shape1, itemsize)
+    size2 = _get_size(shape2, itemsize)
+    if abs(size1 - target_bytes) < abs(size2 - target_bytes):
+        return shape1
+    return shape2
+
+
+def _get_size(shape, itemsize):
+    return np.product(shape) * itemsize
