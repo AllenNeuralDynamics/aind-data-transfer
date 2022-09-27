d765222ea09bf21d7c45da4864205a11dbda02e2
jtyoung84 <104453205+jtyoung84@users.noreply.github.com>
2022-09-27 15:20:32 -0700

    fix: blosc config loader

--- tests/test_configuration_loader.py
+++ tests/test_configuration_loader.py
@@ -1,5 +1,7 @@
 import unittest
+from pathlib import Path
 from transfer.configuration_loader import EphysJobConfigurationLoader
+from numcodecs import Blosc
 
 
 class TestEphysJobConfigs(unittest.TestCase):
@@ -9,15 +11,22 @@ class TestEphysJobConfigs(unittest.TestCase):
         '"input_dir":"some_dir/some_sub_dir"}'
     )
 
-    compressor_configs = (
-        '{"compressor_name":"wavpack",'
-        '"kwargs":{"level":3}}'
-    )
-
     writer_configs = (
         '{"output_dir":"another_dir/zarr_stuff",'
         '"job_kwargs":{"n_jobs":20,"chunk_duration":"1s","progress_bar":true}}'
     )
+
+    expected_read_configs = {
+        'reader_name': 'openephys',
+        'input_dir': Path('some_dir/some_sub_dir')
+    }
+
+    expected_write_configs = {
+        'output_dir': Path('another_dir/zarr_stuff'),
+        'job_kwargs': {'n_jobs': 20,
+                       'chunk_duration': '1s',
+                       'progress_bar': True}
+    }
 
     config_loader = EphysJobConfigurationLoader()
 
@@ -26,25 +35,33 @@ class TestEphysJobConfigs(unittest.TestCase):
             '{"compressor_name":"wavpack",'
             '"kwargs":{"level":3}}'
         )
-        rc, cc, wc = self.config_loader.get_configs(
-            args=['-r', self.reader_configs,
-                  '-c', wavpack_compressor_configs,
-                  '-w', self.writer_configs])
+        expected_wavpack_configs = {
+            'compressor_name': 'wavpack',
+            'kwargs': {'level': 3}
+        }
+        actual_reader_conf, actual_compressor_conf, actual_write_conf = (
+            self.config_loader.get_configs(
+                args=['-r', self.reader_configs,
+                      '-c', wavpack_compressor_configs,
+                      '-w', self.writer_configs])
+        )
 
-        print(rc, cc, wc)
-
-        self.assertTrue(True)
+        self.assertEqual(actual_reader_conf, self.expected_read_configs)
+        self.assertEqual(actual_compressor_conf, expected_wavpack_configs)
+        self.assertEqual(actual_write_conf, self.expected_write_configs)
 
     def test_ephys_job_configs_loader_blosc(self):
         blosc_compressor_configs = (
             '{"compressor_name":"blosc",'
             '"kwargs":{"shuffle":"BITSHUFFLE"}}'
         )
-        rc, cc, wc = self.config_loader.get_configs(
+        expected_blosc_configs = {
+            'compressor_name': 'blosc',
+            'kwargs': {'shuffle': Blosc.BITSHUFFLE}
+        }
+        _, actual_compressor_conf, _ = self.config_loader.get_configs(
             args=['-r', self.reader_configs,
                   '-c', blosc_compressor_configs,
                   '-w', self.writer_configs])
 
-        print(rc, cc, wc)
-
-        self.assertTrue(True)
+        self.assertEqual(actual_compressor_conf, expected_blosc_configs)
