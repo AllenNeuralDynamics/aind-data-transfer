7730f2d1049da284fd9d39bbf3a56ec9fe1a4b19
Alessio Buccino <alejoe9187@gmail.com>
2022-10-11 12:23:46 +0200

    fix: update tests

--- tests/test_configuration_loader.py
+++ tests/test_configuration_loader.py
@@ -25,6 +25,7 @@ class TestEphysJobConfigs(unittest.TestCase):
         )
         expected_configs = {
             "jobs": {
+                "correct_opto": True,
                 "clip": True,
                 "compress": True,
                 "upload_to_s3": True,
