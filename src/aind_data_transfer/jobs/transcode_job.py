d00d4b3d517b359ec499de1137d6d146b42ab623
Cameron Arshadi <arshadic23@gmail.com>
2022-12-13 18:10:57 -0800

    fix transcode job argument parsing

--- src/aind_data_transfer/jobs/transcode_job.py
+++ src/aind_data_transfer/jobs/transcode_job.py
@@ -99,13 +99,13 @@ def _build_ome_zar_cmd(
         f"--scale_factor=2 "
         f"--deployment=slurm"
     )
-    if job_opts['chunk_shape']:
+    if "chunk_shape" in job_opts:
         chunks = " ".join(str(el) for el in job_opts['chunk_shape'])
         job_cmd += f" --chunk_shape {chunks}"
-    if job_opts['exclude']:
+    if "exclude" in job_opts:
         exclusions = " ".join(job_opts['exclude'])
         job_cmd += f" --exclude {exclusions}"
-    if job_opts["resume"]:
+    if "resume" in job_opts and job_opts["resume"]:
         job_cmd += " --resume"
     return job_cmd
 
