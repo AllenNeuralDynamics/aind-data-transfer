90cdf4fde26911004e557579426f43563d6f447e
jtyoung84 <104453205+jtyoung84@users.noreply.github.com>
2022-12-30 17:47:39 -0800

    feat: refactors and adds unit tests

--- src/aind_data_transfer/jobs/s3_upload_job.py
+++ src/aind_data_transfer/jobs/s3_upload_job.py
@@ -5,7 +5,6 @@ import logging
 import os
 import sys
 import tempfile
-from typing import Optional
 
 from aind_codeocean_api.codeocean import CodeOceanClient
 from botocore.exceptions import ClientError
@@ -35,40 +34,41 @@ class GenericS3UploadJob:
         self.args = args
         self.configs = self._load_configs(args)
 
-    def _get_endpoints(self, job_args, s3_region) -> Optional[dict]:
-        endpoints = getattr(job_args, self.SERVICE_ENDPOINT_KEY)
-        if not endpoints:
-            try:
-                s3_secret_name = self.SERVICE_ENDPOINT_KEY
+    @staticmethod
+    def _get_endpoints(service_endpoint_key, s3_region) -> dict:
+        try:
+            s3_secret_name = service_endpoint_key
+            get_secret(s3_secret_name, s3_region)
+            endpoints = json.loads(
                 get_secret(s3_secret_name, s3_region)
-                endpoints = json.loads(get_secret(s3_secret_name, s3_region))
-            except ClientError as e:
-                logging.warning(
-                    f"Unable to retrieve aws secret: "
-                    f"{self.SERVICE_ENDPOINT_KEY}"
-                )
-                logging.debug(e.response)
-                endpoints = {}
+            )
+        except ClientError as e:
+            logging.warning(
+                f"Unable to retrieve aws secret: "
+                f"{service_endpoint_key}"
+            )
+            logging.debug(e.response)
+            endpoints = {}
         return endpoints
 
-    @staticmethod
-    def _upload_subject_metadata(
-        metadata_service_url, subject_id, s3_bucket, s3_prefix, dry_run
-    ) -> None:
+    def _upload_subject_metadata(self) -> None:
+        metadata_service_url = (
+            self.configs.service_endpoints.get("metadata_service_url")
+        )
         if metadata_service_url:
             subject_metadata = SubjectMetadata.ephys_job_to_subject(
                 metadata_service_url=metadata_service_url,
-                subject_id=subject_id,
+                subject_id=self.configs.subject_id,
             )
             file_name = SubjectMetadata.output_file_name
-            final_s3_prefix = "/".join([s3_prefix, file_name])
+            final_s3_prefix = "/".join([self.s3_prefix, file_name])
             with tempfile.NamedTemporaryFile(mode="w") as tmp:
                 tmp.write(json.dumps(subject_metadata, indent=4))
                 copy_to_s3(
                     file_to_upload=tmp.name,
-                    s3_bucket=s3_bucket,
+                    s3_bucket=self.configs.s3_bucket,
                     s3_prefix=final_s3_prefix,
-                    dryrun=dry_run,
+                    dryrun=self.configs.dry_run,
                 )
         else:
             logging.warning(
@@ -78,7 +78,9 @@ class GenericS3UploadJob:
 
     @staticmethod
     def _upload_data_description_metadata(
-        s3_bucket, s3_prefix, dry_run
+            s3_bucket,
+            s3_prefix,
+            dry_run
     ) -> None:
         data_description_metadata = (
             DataDescriptionMetadata.get_data_description(name=s3_prefix)
@@ -95,7 +97,9 @@ class GenericS3UploadJob:
                 dryrun=dry_run,
             )
 
-    def _get_codeocean_client(self, s3_region, codeocean_domain):
+    def _get_codeocean_client(self,
+                              s3_region,
+                              codeocean_domain):
         # Try to see if it's been set by an env var
         co_api_token = os.getenv(
             f"{self.CODEOCEAN_TOKEN_KEY.replace('_', '-').upper()}"
@@ -124,7 +128,9 @@ class GenericS3UploadJob:
         return codeocean_client
 
     @staticmethod
-    def _trigger_codeocean_capsule(codeocean_client, capsule_id, dry_run):
+    def _trigger_codeocean_capsule(codeocean_client,
+                                   capsule_id,
+                                   dry_run):
         if codeocean_client and capsule_id:
             logging.info("Triggering capsule run.")
             if not dry_run:
@@ -145,14 +151,13 @@ class GenericS3UploadJob:
                 "CodeOcean endpoints are required to trigger capsule."
             )
 
-    @staticmethod
-    def _create_s3_prefix(
-        modality: str, subject_id: str, acq_date: str, acq_time: str
-    ) -> str:
+    @property
+    def s3_prefix(self):
         # Validate date and time strings
         try:
             datetime.datetime.strptime(
-                acq_date + " " + acq_time, "%Y-%m-%d %H-%M-%S"
+                self.configs.acq_date + " " + self.configs.acq_time,
+                "%Y-%m-%d %H-%M-%S"
             )
         except ValueError:
             raise ValueError(
@@ -160,7 +165,10 @@ class GenericS3UploadJob:
                 "yyyy-MM-dd and acq_time should be HH-mm-SS"
             )
 
-        return "_".join([modality, subject_id, acq_date, acq_time])
+        return "_".join(
+            [self.configs.modality, self.configs.subject_id,
+             self.configs.acq_date, self.configs.acq_time]
+        )
 
     def _load_configs(self, args: list) -> argparse.Namespace:
         parser = argparse.ArgumentParser()
@@ -170,68 +178,60 @@ class GenericS3UploadJob:
         parser.add_argument("-m", "--modality", required=True, type=str)
         parser.add_argument("-a", "--acq-date", required=True, type=str)
         parser.add_argument("-t", "--acq-time", required=True, type=str)
-        service_endpoints_name = self.SERVICE_ENDPOINT_KEY.replace("_", "-")
         parser.add_argument(
-            "-e", f"--{service_endpoints_name}", required=False, type=str
+            "-e", "--service-endpoints", required=False, type=json.loads
         )
         parser.add_argument("-r", "--s3-region", required=False, type=str)
         parser.add_argument("--dry-run", action="store_true")
         parser.set_defaults(dry_run=False)
         parser.set_defaults(s3_region=self.S3_DEFAULT_REGION)
-
         job_args = parser.parse_args(args)
+        if not job_args.service_endpoints:
+            job_args.service_endpoints = (
+                self._get_endpoints(self.SERVICE_ENDPOINT_KEY,
+                                    job_args.s3_region
+                                    )
+            )
         return job_args
 
     def run_job(self) -> None:
 
-        job_args = self._load_configs(self.args)
-        s3_region = job_args.s3_region
+        s3_region = self.configs.s3_region
 
-        s3_prefix = self._create_s3_prefix(
-            job_args.modality,
-            job_args.subject_id,
-            job_args.acq_date,
-            job_args.acq_time,
-        )
-        data_prefix = "/".join([s3_prefix, job_args.modality])
+        data_prefix = "/".join([self.s3_prefix, self.configs.modality])
 
         upload_to_s3(
-            directory_to_upload=job_args.data_source,
-            s3_bucket=job_args.s3_bucket,
+            directory_to_upload=self.configs.data_source,
+            s3_bucket=self.configs.s3_bucket,
             s3_prefix=data_prefix,
-            dryrun=job_args.dry_run,
+            dryrun=self.configs.dry_run,
         )
-
-        endpoints = self._get_endpoints(job_args, s3_region)
 
         # Create subject.json file if metadata service url is provided
-        self._upload_subject_metadata(
-            metadata_service_url=(
-                endpoints.get(self.METADATA_SERVICE_URL_KEY)
-            ),
-            subject_id=job_args.subject_id,
-            s3_bucket=job_args.s3_bucket,
-            s3_prefix=s3_prefix,
-            dry_run=job_args.dry_run,
-        )
+        self._upload_subject_metadata()
 
         # Create data description file
         self._upload_data_description_metadata(
-            s3_bucket=job_args.s3_bucket,
-            s3_prefix=s3_prefix,
-            dry_run=job_args.dry_run,
+            s3_bucket=self.configs.s3_bucket,
+            s3_prefix=self.s3_prefix,
+            dry_run=self.configs.dry_run,
         )
 
         # Register to code ocean if url is provided
         codeocean_client = self._get_codeocean_client(
             s3_region=s3_region,
-            codeocean_domain=endpoints.get("codeocean_domain"),
+            codeocean_domain=self.configs.service_endpoints.get(
+                "codeocean_domain"
+            ),
         )
         self._trigger_codeocean_capsule(
             codeocean_client=codeocean_client,
-            capsule_id=endpoints.get(self.CODEOCEAN_CAPSULE_KEY),
-            dry_run=job_args.dry_run,
+            capsule_id=self.configs.service_endpoints.get(
+                self.CODEOCEAN_CAPSULE_KEY
+            ),
+            dry_run=self.configs.dry_run,
         )
+
 
 if __name__ == "__main__":
     sys_args = sys.argv[1:]
--- tests/test_generic_upload_job.py
+++ tests/test_generic_upload_job.py
@@ -1,51 +1,146 @@
 import unittest
+from io import StringIO
+from unittest.mock import patch, Mock
+import json
+from botocore.exceptions import ClientError
 from aind_data_transfer.jobs.s3_upload_job import GenericS3UploadJob
 
 
 class TestProcessingMetadata(unittest.TestCase):
 
-    def test_create_s3_prefix(self):
-        modality = "ecephys"
-        subject_id = "12345"
-        acq_date = "2020-10-10"
-        acq_time = "13-24-01"
+    fake_endpoints_str = (
+            '{"metadata_service_url": "http://metada_service_url",'
+            '"codeocean_domain": "https://codeocean.acme.org",'
+            '"codeocean_trigger_capsule": "abc-123",'
+            '"codeocean-api-token": "some_token"}'
+        )
+    args1 = (
+        ["-d", "some_dir", "-b", "some_s3_bucket", "-s", "12345",
+         "-m", "ecephys", "-a", "2022-10-10", "-t", "13-24-01",
+         "-e", fake_endpoints_str, "--dry-run"]
+    )
 
-        expected_s3_prefix = "ecephys_12345_2020-10-10_13-24-01"
-        actual_s3_prefix = GenericS3UploadJob._create_s3_prefix(modality,
-                                                                subject_id,
-                                                                acq_date,
-                                                                acq_time)
+    def test_create_s3_prefix(self):
+        job = GenericS3UploadJob(self.args1)
+
+        expected_s3_prefix = "ecephys_12345_2022-10-10_13-24-01"
+        actual_s3_prefix = job.s3_prefix
         self.assertEqual(expected_s3_prefix, actual_s3_prefix)
 
     def test_create_s3_prefix_error(self):
-        modality = "ecephys"
-        subject_id = "12345"
+        bad_args = self.args1.copy()
 
         with self.assertRaises(ValueError):
-            GenericS3UploadJob._create_s3_prefix(modality,
-                                                 subject_id,
-                                              "2020-13-10",  # Bad month
-                                              "12-01-01")
-        with self.assertRaises(ValueError):
-            GenericS3UploadJob._create_s3_prefix(modality,
-                                                 subject_id,
-                                              "2020-12-32",  # Bad day
-                                              "12-01-01")
-        with self.assertRaises(ValueError):
-            GenericS3UploadJob._create_s3_prefix(modality,
-                                                 subject_id,
-                                              "2020-12-31",
-                                              "24-60-01")  # Bad hour
+            bad_args[9] = "2020-13-10"  # Bad Month
+            GenericS3UploadJob(bad_args).s3_prefix()
 
         with self.assertRaises(ValueError):
-            GenericS3UploadJob._create_s3_prefix(modality,
-                                                 subject_id,
-                                              "2020-12-31",
-                                              "12-60-01")  # Bad minute
+            bad_args[9] = "2020-12-32"  # Bad Day
+            GenericS3UploadJob(bad_args).s3_prefix()
+
         with self.assertRaises(ValueError):
-            GenericS3UploadJob._create_s3_prefix(modality,
-                                                 subject_id,
-                                              "2020-12-31",
-                                              "12-59-60")  # Bad second
+            bad_args[9] = "2020-12-31"
+            bad_args[11] = "24-59-01"  # Bad Hour
+            GenericS3UploadJob(bad_args).s3_prefix()
+
+        with self.assertRaises(ValueError):
+            bad_args[11] = "12-60-01"  # Bad Minute
+            GenericS3UploadJob(bad_args).s3_prefix()
+
+        with self.assertRaises(ValueError):
+            bad_args[11] = "12-59-60"  # Bad Second
+            GenericS3UploadJob(bad_args).s3_prefix()
+
+    @patch('sys.stderr', new_callable=StringIO)
+    def test_load_configs(self, mock_stderr):
+        job = GenericS3UploadJob(self.args1)
+        expected_configs_vars = (
+            {"data_source": "some_dir",
+             "s3_bucket": "some_s3_bucket",
+             "subject_id": "12345",
+             "modality": "ecephys",
+             "acq_date": "2022-10-10",
+             "acq_time": "13-24-01",
+             "s3_region": "us-west-2",
+             "service_endpoints": json.loads(self.fake_endpoints_str),
+             "dry_run": True}
+        )
+        self.assertEqual(expected_configs_vars, vars(job.configs))
+
+        missing_arg = (
+            ["-d", "some_dir", "-b", "some_s3_bucket", "-s", "12345",
+             "-m", "ecephys", "-a", "2022-10-10"]
+        )
+
+        with self.assertRaises(SystemExit):
+            GenericS3UploadJob(missing_arg)
+        self.assertRegexpMatches(
+            mock_stderr.getvalue(),
+            r"the following arguments are required: -t/--acq-time"
+        )
+
+    @patch('logging.Logger.warning')
+    @patch("boto3.session.Session")
+    def test_get_endpoints(self, mock_session, mock_log):
+        job = GenericS3UploadJob(self.args1)
+        expected_endpoints1 = json.loads(self.fake_endpoints_str)
+        self.assertEqual(expected_endpoints1, job.configs.service_endpoints)
+
+        # Check job loads endpoints from s3 secrets
+        args_without_endpoints = (
+            ["-d", "some_dir", "-b", "some_s3_bucket", "-s", "12345",
+             "-m", "ecephys", "-a", "2022-10-10", "-t", "10-10-00",
+             "--dry-run"]
+        )
+        mock_session_object = Mock()
+        mock_client = Mock()
+        mock_client.get_secret_value.return_value = ({
+            'SecretString': self.fake_endpoints_str
+        })
+        mock_session_object.client.return_value = mock_client
+        mock_session.return_value = mock_session_object
+        job2 = GenericS3UploadJob(args_without_endpoints)
+        self.assertEqual(expected_endpoints1, job2.configs.service_endpoints)
+
+        # Check endpoints are empty if not in sys args or in aws
+        mock_client.get_secret_value.side_effect = Mock(
+            side_effect=ClientError(
+                error_response=({
+                    'Error': {'Code': '500', 'Message': 'Error'}
+                }),
+                operation_name=None)
+        )
+        job3 = GenericS3UploadJob(args_without_endpoints)
+        self.assertEqual({}, job3.configs.service_endpoints)
+        mock_log.assert_called_with(
+            f"Unable to retrieve aws secret: {job3.SERVICE_ENDPOINT_KEY}"
+        )
+
+    @patch('aind_data_transfer.jobs.s3_upload_job.copy_to_s3')
+    @patch('logging.Logger.warning')
+    @patch(
+        'aind_data_transfer.transformations.metadata_creation.SubjectMetadata.'
+        'ephys_job_to_subject'
+    )
+    @patch('tempfile.NamedTemporaryFile')
+    def test_upload_subject_metadata(self,
+                                     mocked_tempfile,
+                                     mocked_ephys_job_to_subject,
+                                     mock_log,
+                                     mock_copy_to_s3):
+        mocked_ephys_job_to_subject.return_value = {}
+        job = GenericS3UploadJob(self.args1)
+        job._upload_subject_metadata()
+        mock_copy_to_s3.assert_called_once()
+        mocked_tempfile.assert_called_once()
+
+        # Check warning message if not metadata url is found
+        empty_args = self.args1.copy()
+        empty_args[13] = '{}'
+        job2 = GenericS3UploadJob(empty_args)
+        job2._upload_subject_metadata()
+        mock_log.assert_called_with(
+            "No metadata service url given. Not able to get subject metadata."
+        )
 
 
