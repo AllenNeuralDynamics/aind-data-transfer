3a40883a7b33820b9ea5246b568a3129a61a66c2
Cameron Arshadi <arshadic23@gmail.com>
2023-01-09 18:06:05 -0800

    update create_neuroglancer_links.py

--- scripts/create_neuroglancer_links.py
+++ scripts/create_neuroglancer_links.py
@@ -1,99 +1,118 @@
+import argparse
 import re
 
-import s3fs
+import fsspec
+import zarr
 from ng_link import NgState
 
+from aind_data_transfer.util import file_utils
 
-def map_channel_colors(channels):
-    colors = ['green', 'red', 'blue', 'yellow']
+TILE_PATTERN = r"tile_x_\d{4}_y_\d{4}_z_\d{4}_ch_\d+"
+CHANNEL_PATTERN = r"ch_(\d+)"
+
+
+def _map_channel_colors(channels):
+    colors = ["green", "magenta", "cyan", "yellow"]
     if len(channels) > 4:
         raise Exception("Only up to 4 channels supported.")
     color_map = {channels[i]: colors[i] for i in range(len(channels))}
     return color_map
 
 
-def main():
-    zarr_path = r"s3://aind-open-data/exaSPIM_614978_2022-10-21_12-27-00/exaSPIM/"
-
-    tile_pattern = r"tile_x_\d{4}_y_\d{4}_z_\d{4}_ch_\d+"
-    channel_pattern = r"ch_(\d+)"
-
+def _parse_dimensions(zarr_path):
+    z = zarr.open(zarr_path, "r")
+    attrs = dict(z[next(iter(z.keys()))].attrs)
+    ms = attrs["multiscales"][0]
+    axes = {el["name"]: el for el in ms["axes"]}
+    full_res = [ds for ds in ms["datasets"] if ds["path"] == "0"][0]
+    scale = [
+        ct
+        for ct in full_res["coordinateTransformations"]
+        if ct["type"] == "scale"
+    ][0]
+    voxel_size = scale["scale"]
     dimensions = {
-        # check the order
-        "x": {
-            "voxel_size": 0.75,
-            "unit": 'microns'
-        },
-        "y": {
-            "voxel_size": 0.75,
-            "unit": 'microns'
-        },
-        "z": {
-            "voxel_size": 1.0,
-            "unit": 'microns'
-        },
-        "t": {
-            "voxel_size": 0.001,
-            "unit": 'seconds'
-        },
-        'c': {
-            "voxel_size": 1.0,
-            "unit": "microns"
-        }
+        "x": {"voxel_size": voxel_size[4], "unit": axes["x"]["unit"]},
+        "y": {"voxel_size": voxel_size[3], "unit": axes["y"]["unit"]},
+        "z": {"voxel_size": voxel_size[2], "unit": axes["z"]["unit"]},
+        "c'": {"voxel_size": voxel_size[1], "unit": ""},
+        "t": {"voxel_size": voxel_size[0], "unit": axes["t"]["unit"]},
     }
+    return dimensions
 
-    fs = s3fs.S3FileSystem()
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input", type=str, help="Path to the dataset")
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=str,
+        help="directory to output the process_output.json file",
+    )
+    parser.add_argument(
+        "--vmin", type=float, default=0, help="display range minimum"
+    )
+    parser.add_argument(
+        "--vmax", type=float, default=500.0, help="display range maximum"
+    )
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+
+    provider, bucket, cloud_path = file_utils.parse_cloud_url(args.input)
+    protocol = provider.replace("://", "")
+
+    fs = fsspec.filesystem(protocol)
+
+    dimensions = _parse_dimensions(args.input)
+
     channels = []
     source_paths = []
-    for f in fs.listdir(zarr_path):
-        m = re.search(tile_pattern, f['Key'])
+    for f in fs.listdir(args.input):
+        m = re.search(TILE_PATTERN, f["Key"])
         if m is None:
             continue
-        tile_source = "zarr://s3://" + f['Key']
-        print(tile_source)
+        tile_source = provider + f["Key"]
         source_paths.append(tile_source)
         tile_name = m.group(0)
-        m = re.search(channel_pattern, tile_name)
+        m = re.search(CHANNEL_PATTERN, tile_name)
         channel = int(m.group(1))
         channels.append(channel)
 
     unique_channels = set(channels)
-    color_map = map_channel_colors(list(unique_channels))
+    color_map = _map_channel_colors(list(unique_channels))
 
     layers = []
     for source, ch in zip(source_paths, channels):
         layer = {
-            'source': source,
-            # 'channel': channels.index(ch),
+            "source": source,
             "tab": "rendering",
             "blend": "additive",
-            'shader': {
-                'color': color_map[ch],
-                'emitter': 'RGB',
-                'vec': 'vec3'
+            "shader": {
+                "color": color_map[ch],
+                "emitter": "RGB",
+                "vec": "vec3",
             },
-            'shaderControls': {  # Optional
-                "normalized": {
-                    "range": [30, 50]
-                }
+            "shaderControls": {  # Optional
+                "normalized": {"range": [args.vmin, args.vmax]}
             },
         }
         layers.append(layer)
 
-    state = {
-        "dimensions": dimensions,
-        "layers": layers
-    }
+    state = {"dimensions": dimensions, "layers": layers}
 
     neuroglancer_link = NgState(
         input_config=state,
-        mount_service='s3',
-        bucket_path='aind-open-data/exaSPIM_614978_2022-10-21_12-27-00',
-        output_json="s3://aind-open-data/exaSPIM_614978_2022-10-21_12-27-00/ng-link.json",
+        mount_service=protocol,
+        bucket_path=bucket,
+        output_json=args.output,
     )
 
     neuroglancer_link.save_state_as_json()
-    #print(neuroglancer_link.get_url_link())
+
 
 if __name__ == "__main__":
     main()
