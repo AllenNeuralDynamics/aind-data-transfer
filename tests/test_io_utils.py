1ab73667eaa2079057007d22fd2508873cb8db65
Cameron Arshadi <arshadic23@gmail.com>
2023-02-08 19:52:33 -0800

    feat add tests

--- tests/test_io_utils.py
+++ tests/test_io_utils.py
@@ -7,13 +7,13 @@ import dask.array
 import h5py
 import numpy as np
 import zarr.core
-from distributed import Client
 from tifffile import tifffile
 
 from aind_data_transfer.util.io_utils import (
     DataReaderFactory,
     ImarisReader,
     TiffReader,
+    ScanImageTiffReader
 )
 
 # TODO: make test fixtures instead of constants?
@@ -41,6 +41,51 @@ def _write_test_h5(folder, n=1):
         with h5py.File(path, "w") as f:
             f.create_dataset(DEFAULT_DATA_PATH, data=a, chunks=True)
     return paths
+
+
+class TestScanImageTiffReader(unittest.TestCase):
+    def setUp(self):
+        self._temp_dir = tempfile.TemporaryDirectory()
+        self._image_dir = Path(self._temp_dir.name) / "images"
+        os.makedirs(self._image_dir, exist_ok=True)
+        self._image_path = _write_test_tiffs(self._image_dir, n=1)[0]
+        self._reader = ScanImageTiffReader(self._image_path)
+
+    def tearDown(self) -> None:
+        self._reader.close()
+        self._temp_dir.cleanup()
+
+    def test_get_filepath(self):
+        self.assertEqual(self._image_path, self._reader.get_filepath())
+
+    def test_as_array(self):
+        a = self._reader.as_array()
+        self.assertIsInstance(a, np.ndarray)
+        self.assertEqual(IM_SHAPE, a.shape)
+        self.assertTrue(np.all(a == 1))
+        self.assertEqual(IM_DTYPE, a.dtype)
+
+    def test_as_dask_array(self):
+        d = self._reader.as_dask_array()
+        self.assertIsInstance(d, dask.array.Array)
+        self.assertEqual(IM_SHAPE, d.shape)
+        self.assertTrue(np.all(d == 1))
+        self.assertEqual(IM_DTYPE, d.dtype)
+
+    def test_get_shape(self):
+        self.assertEqual(IM_SHAPE, self._reader.get_shape())
+
+    def test_get_chunks(self):
+        chunks = self._reader.get_chunks()
+        self.assertEqual(3, len(chunks))
+        self.assertTrue(all(isinstance(c, int) for c in chunks))
+        self.assertTrue(all(c >= 1 for c in chunks))
+
+    def test_get_dtype(self):
+        self.assertEqual(IM_DTYPE, self._reader.get_dtype())
+
+    def test_get_itemsize(self):
+        self.assertEqual(2, self._reader.get_itemsize())
 
 
 class TestTiffReader(unittest.TestCase):
@@ -111,12 +156,10 @@ class TestHDF5Reader(unittest.TestCase):
         os.makedirs(self._image_dir, exist_ok=True)
         self._image_path = _write_test_h5(self._image_dir, n=1)[0]
         self._reader = ImarisReader(self._image_path)
-        self.client = Client()
 
     def tearDown(self) -> None:
         self._reader.close()
         self._temp_dir.cleanup()
-        self.client.close()
 
     def test_constructor(self):
         self.assertEqual(self._reader.filepath, self._image_path)
@@ -136,9 +179,9 @@ class TestHDF5Reader(unittest.TestCase):
     # def test_as_dask_array(self):
     #     d = self._reader.as_dask_array()
     #     self.assertIsInstance(d, dask.array.Array)
-    #     self.assertEqual(SHAPE, d.shape)
+    #     self.assertEqual(IM_SHAPE, d.shape)
     #     self.assertTrue(np.all(d == 1))
-    #     self.assertEqual(DTYPE, d.dtype)
+    #     self.assertEqual(IM_DTYPE, d.dtype)
 
     def test_get_shape(self):
         self.assertEqual(IM_SHAPE, self._reader.get_shape())
@@ -178,7 +221,7 @@ class TestDataReaderFactor(unittest.TestCase):
 
     def test_create_tiffreader(self):
         reader = DataReaderFactory().create(self._tiff_path)
-        self.assertIsInstance(reader, TiffReader)
+        self.assertIsInstance(reader, ScanImageTiffReader)
 
 
 if __name__ == "__main__":
