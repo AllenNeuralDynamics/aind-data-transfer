5494368b3c92d803720f11df56c694760f2e9e61
Cameron Arshadi <arshadic23@gmail.com>
2023-02-10 18:59:22 -0800

    use kerchunk for tiff reading (WIP)
    - this seems to work fine, but  the tests currently fail for reasons not yet understood

--- tests/test_io_utils.py
+++ tests/test_io_utils.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import tempfile
 import unittest
 from pathlib import Path
@@ -6,14 +7,12 @@ from pathlib import Path
 import dask.array
 import h5py
 import numpy as np
-import zarr.core
 from tifffile import tifffile
 
 from aind_data_transfer.util.io_utils import (
     DataReaderFactory,
     ImarisReader,
     TiffReader,
-    ScanImageTiffReader
 )
 
 # TODO: make test fixtures instead of constants?
@@ -43,17 +42,16 @@ def _write_test_h5(folder, n=1):
     return paths
 
 
-class TestScanImageTiffReader(unittest.TestCase):
+class TestTiffReader(unittest.TestCase):
     def setUp(self):
-        self._temp_dir = tempfile.TemporaryDirectory()
-        self._image_dir = Path(self._temp_dir.name) / "images"
-        os.makedirs(self._image_dir, exist_ok=True)
+        self._image_dir = Path(__file__).parent / "resources/imaging/data/tiff"
+        self._image_dir.mkdir(parents=True, exist_ok=True)
         self._image_path = _write_test_tiffs(self._image_dir, n=1)[0]
-        self._reader = ScanImageTiffReader(self._image_path)
+        self._reader = TiffReader(self._image_path)
 
     def tearDown(self) -> None:
         self._reader.close()
-        self._temp_dir.cleanup()
+        shutil.rmtree(self._image_dir)
 
     def test_get_filepath(self):
         self.assertEqual(self._image_path, self._reader.get_filepath())
@@ -86,67 +84,6 @@ class TestScanImageTiffReader(unittest.TestCase):
 
     def test_get_itemsize(self):
         self.assertEqual(2, self._reader.get_itemsize())
-
-
-class TestTiffReader(unittest.TestCase):
-    def setUp(self):
-        self._temp_dir = tempfile.TemporaryDirectory()
-        self._image_dir = Path(self._temp_dir.name) / "images"
-        os.makedirs(self._image_dir, exist_ok=True)
-        self._image_path = _write_test_tiffs(self._image_dir, n=1)[0]
-        self._reader = TiffReader(self._image_path)
-
-    def tearDown(self) -> None:
-        self._reader.close()
-        self._temp_dir.cleanup()
-
-    def test_constructor(self):
-        self.assertEqual(self._reader.filepath, self._image_path)
-        self.assertIsNotNone(self._reader.get_handle())
-
-    def test_get_filepath(self):
-        self.assertEqual(self._image_path, self._reader.get_filepath())
-
-    def test_as_array(self):
-        a = self._reader.as_array()
-        self.assertIsInstance(a, np.ndarray)
-        self.assertEqual(IM_SHAPE, a.shape)
-        self.assertTrue(np.all(a == 1))
-        self.assertEqual(IM_DTYPE, a.dtype)
-
-    def test_as_dask_array(self):
-        d = self._reader.as_dask_array()
-        self.assertIsInstance(d, dask.array.Array)
-        self.assertEqual(IM_SHAPE, d.shape)
-        self.assertTrue(np.all(d == 1))
-        self.assertEqual(IM_DTYPE, d.dtype)
-
-    def test_as_zarr(self):
-        z = self._reader.as_zarr()
-        self.assertIsInstance(z, zarr.core.Array)
-        self.assertEqual(IM_SHAPE, z.shape)
-        self.assertTrue(np.all(z[:] == 1))
-        self.assertEqual(IM_DTYPE, z.dtype)
-
-    def test_get_shape(self):
-        self.assertEqual(IM_SHAPE, self._reader.get_shape())
-
-    def test_get_chunks(self):
-        chunks = self._reader.get_chunks()
-        self.assertEqual(3, len(chunks))
-        self.assertTrue(all(isinstance(c, int) for c in chunks))
-        self.assertTrue(all(c >= 1 for c in chunks))
-
-    def test_get_itemsize(self):
-        self.assertEqual(2, self._reader.get_itemsize())
-
-    def test_get_handle(self):
-        self.assertIsNotNone(self._reader.get_handle())
-
-    def test_close(self):
-        self._reader.close()
-        self.assertIsNone(self._reader.handle)
-        self.assertRaises(AttributeError, self._reader.as_array)
 
 
 class TestHDF5Reader(unittest.TestCase):
@@ -221,7 +158,7 @@ class TestDataReaderFactor(unittest.TestCase):
 
     def test_create_tiffreader(self):
         reader = DataReaderFactory().create(self._tiff_path)
-        self.assertIsInstance(reader, ScanImageTiffReader)
+        self.assertIsInstance(reader, TiffReader)
 
 
 if __name__ == "__main__":
