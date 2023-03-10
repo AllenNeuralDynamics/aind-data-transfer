9d99620efdeefcdc4543175d3257e97ed3f9c4d1
jtyoung84 <104453205+jtyoung84@users.noreply.github.com>
2023-03-09 17:31:22 -0800

    feat: refactors metadata creation

--- src/aind_data_transfer/transformations/metadata_creation.py
+++ src/aind_data_transfer/transformations/metadata_creation.py
@@ -1,68 +1,292 @@
 """This module will have classes that handle mapping to metadata files."""
-import logging
-import re
-from datetime import datetime
-from typing import Optional
 import json
+import logging
+import os
+from abc import ABC, abstractmethod
+from datetime import datetime
+from pathlib import Path
+from typing import Optional, Tuple, Type
 
-import pydantic
+import aind_data_schema.base
 from aind_data_schema.data_description import (
     Funding,
     Institution,
     RawDataDescription,
 )
+from aind_data_schema.procedures import Procedures
 from aind_data_schema.processing import DataProcess, Processing, ProcessName
 from aind_data_schema.subject import Subject
-from aind_data_schema.procedures import Procedures
 from aind_metadata_service.client import AindMetadataServiceClient
+from pydantic import validate_model
+from requests import Response
 
 import aind_data_transfer
-from pathlib import Path
 
 
-class ProcessingMetadata:
-    """Class to handle the creation of the processing metadata file."""
+class MetadataCreation(ABC):
+    """Abstract class with convenient methods to handle metadata creation."""
 
-    # TODO: import this from aind_data_schema.Processing class
-    output_file_name = Processing.construct().default_filename()
+    def __init__(self, model_obj: Optional[dict] = None):
+        """
+        Default class constructor.
+        Parameters
+        ----------
+        model_obj : Optional[dict]
+          The metadata as a dict object. We'll use this representation to
+          avoid potential dependency conflicts from aind_data_schema. We can
+          validate any of the incoming data using the version of
+          aind_data_schema attached to aind_data_transfer.
+        """
+        self.model_obj = model_obj
+
+    @property
+    @abstractmethod
+    def _model(self) -> Type[aind_data_schema.base.AindCoreModel]:
+        """
+        Returns
+        -------
+          Needs to return an AindCoreModel, such as Subject for example.
+        """
+        pass
+
+    @property
+    def output_filename(self):
+        """Returns the default json file name for the model as defined in
+        aind_data_schema."""
+        return self._model.construct().default_filename()
+
+    @classmethod
+    def from_file(cls, file_location: Path):
+        """
+        Construct a MetadataCreation instance from a json file.
+        Parameters
+        ----------
+        file_location : Path
+          Location of the json file
+
+        """
+        with open(file_location) as f:
+            contents = json.load(f)
+        return cls(model_obj=contents)
+
+    def validate_obj(self) -> bool:
+        """
+        Validate the model object. Logs a warning if the model_obj is not
+        valid.
+        Returns
+        -------
+        bool
+          True if the model is valid. False otherwise.
+
+        """
+        *_, validation_error = validate_model(self._model, self.model_obj)
+        if validation_error:
+            logging.warning(f"Validation Errors: {validation_error}")
+            return False
+        else:
+            logging.info("Model is valid.")
+            return True
+
+    def write_to_json(self, path: Path) -> None:
+        """
+        Write the model_obj to a json file. If the Path is a directory, it will
+        use the output_filename method to generate the filename.
+        Parameters
+        ----------
+        path : Path
+          Location of where to save the json file. Can be a directory or file.
+
+        Returns
+        -------
+        None
+
+        """
+
+        if os.path.isdir(path):
+            out_path = path / self.output_filename
+        else:
+            out_path = path
+
+        with open(out_path, "w") as outfile:
+            outfile.write(json.dumps(self.model_obj, indent=3))
+
+
+class ServiceMetadataCreation(MetadataCreation):
+    """Abstract class for metadata pulled from aind_metadata_service"""
+
+    def __init__(self, model_obj: Optional[dict] = None):
+        """
+        Default class constructor.
+        Parameters
+        ----------
+        model_obj : Optional[dict]
+          The metadata as a dict object. We'll use this representation to
+          avoid potential dependency conflicts from aind_data_schema. We can
+          validate any of the incoming data using the version of
+          aind_data_schema attached to aind_data_transfer.
+        """
+        super().__init__(model_obj=model_obj)
 
     @staticmethod
-    def ephys_job_to_processing(
+    @abstractmethod
+    def _get_service_response(
+        subject_id: str,
+        ams_client: AindMetadataServiceClient,
+    ) -> Response:
+        """
+        Abstract method to retrieve metadata from the service.
+        Parameters
+        ----------
+        subject_id : str
+          ID of the subject we want to get metadata for.
+        ams_client : AindMetadataServiceClient
+          A client to connect to aind_metadata_service.
+
+        Returns
+        -------
+        Response
+          Has a status code and json contents.
+
+        """
+        pass
+
+    @classmethod
+    def from_service(cls, subject_id: str, domain: str):
+        """
+        Build the class from data pulled from aind_metadata_service.
+        Parameters
+        ----------
+        subject_id : str
+          ID of the subject we want to get metadata for.
+        domain : str
+          Domain name for aind_metadata_service
+
+        """
+        ams_client = AindMetadataServiceClient(domain=domain)
+        response = cls._get_service_response(
+            subject_id=subject_id, ams_client=ams_client
+        )
+        response_json = response.json()
+        status_code = response.status_code
+        # Everything is okay
+        if status_code == 200:
+            contents = response_json["data"]
+        # Multiple items were found
+        elif status_code == 300:
+            logging.warning(response_json["message"])
+            contents = response_json["data"][0]
+        # The data retrieved is invalid
+        elif status_code == 406:
+            logging.warning(response_json["message"])
+            contents = response_json["data"]
+        # Connected to the service, but no data was found
+        elif status_code == 404:
+            logging.warning(response_json["message"])
+            contents = response_json["data"]
+        # A serious error happened. Build a default model.
+        else:
+            logging.error(response_json["message"])
+            contents = json.loads(Procedures.construct().json())
+        return cls(model_obj=contents)
+
+
+class SubjectMetadata(ServiceMetadataCreation):
+    """Class to manage building the subject metadata"""
+
+    @property
+    def _model(self) -> Type[aind_data_schema.base.AindCoreModel]:
+        return Subject
+
+    @staticmethod
+    def _get_service_response(
+        subject_id: str, ams_client: AindMetadataServiceClient
+    ) -> Response:
+        """
+        Method to retrieve metadata from the service.
+        Parameters
+        ----------
+        subject_id : str
+          ID of the subject we want to get metadata for.
+        ams_client : AindMetadataServiceClient
+          A client to connect to aind_metadata_service.
+
+        Returns
+        -------
+        Response
+          Has a status code and json contents.
+
+        """
+        return ams_client.get_subject(subject_id)
+
+
+class ProceduresMetadata(ServiceMetadataCreation):
+    @property
+    def _model(self) -> Type[aind_data_schema.base.AindCoreModel]:
+        return Procedures
+
+    @staticmethod
+    def _get_service_response(
+        subject_id: str, ams_client: AindMetadataServiceClient
+    ) -> Response:
+        """
+        Method to retrieve metadata from the service.
+        Parameters
+        ----------
+        subject_id : str
+          ID of the subject we want to get metadata for.
+        ams_client : AindMetadataServiceClient
+          A client to connect to aind_metadata_service.
+
+        Returns
+        -------
+        Response
+          Has a status code and json contents.
+
+        """
+        return ams_client.get_procedures(subject_id)
+
+
+class ProcessingMetadata(MetadataCreation):
+    @property
+    def _model(self) -> Type[aind_data_schema.base.AindCoreModel]:
+        return Processing
+
+    @classmethod
+    def from_inputs(
+        cls,
+        process_name: ProcessName,
         start_date_time: datetime,
         end_date_time: datetime,
         input_location: str,
         output_location: str,
         code_url: str,
         parameters: dict,
-        notes: str = None,
-    ) -> Processing:
+        notes: Optional[str] = None,
+    ):
         """
-        Creates a processing instance based on the openephys_job settings
+        Build a ProcessingMetadata instance using some basic parameters.
         Parameters
         ----------
+        process_name : ProcessName
+          Name of the process
         start_date_time : datetime
-          Time the processing job started
+          Start date and time of the process
         end_date_time : datetime
-          Time the processing job ended
+          End date and time of the process
         input_location : str
-          Location of the raw data source
+          Location of the files that are being processed
         output_location : str
-          Location of the processed data
+          Location of the files that are being processed
         code_url : str
-          Location of where the code is maintained.
-          TODO: Extract this from pyproject.toml file.
+          Location of the processing code
         parameters : dict
-          Parameters of the processing job
+          Parameters used in the process
         notes : Optional[str]
-            Optional notes to attach
-        Returns
-        -------
-        aind_data_schema.Processing
-          A Processing instance to annotate a dataset with.
+          Optional notes. Defaults to None.
 
         """
         data_processing_instance = DataProcess(
-            name=ProcessName.EPHYS_PREPROCESSING.value,
+            name=process_name.value,
             version=aind_data_transfer.__version__,
             start_date_time=start_date_time,
             end_date_time=end_date_time,
@@ -75,35 +299,37 @@ class ProcessingMetadata:
         processing_instance = Processing(
             data_processes=[data_processing_instance]
         )
+        return cls(model_obj=processing_instance.dict())
 
-        return processing_instance
 
+class RawDataDescriptionMetadata(MetadataCreation):
+    """Class to handle the creation of the raw data description metadata
+    file."""
 
-class RawDataDescriptionMetadata:
-    """Class to handle the creation of the processing metadata file."""
+    @property
+    def _model(self) -> Type[aind_data_schema.base.AindCoreModel]:
+        return RawDataDescription
 
-    output_file_name = RawDataDescription.construct().default_filename()
-
-    @staticmethod
-    def get_data_description(
+    @classmethod
+    def from_inputs(
+        cls,
         name: str,
-        institution=Institution.AIND,
-        funding_source=(Funding(funder=Institution.AIND.value),),
-    ) -> RawDataDescription:
+        institution: Optional[Institution] = Institution.AIND,
+        funding_source: Optional[Tuple] = (
+                Funding(funder=Institution.AIND.value),
+        ),
+    ):
         """
-        Creates a data description instance
+        Build a RawDataDescriptionMetadata instance using some basic
+        parameters.
         Parameters
         ----------
         name : str
-          Name of data, also the name of the directory containing all metadata
-        institution : str
-          The name of the organization that collected this data
-        funding_source : tuple
-          Funding sources. If internal label as Institution
-        Returns
-        -------
-        aind_data_schema.RawDataDescription
-          A RawDataDescription instance to annotate a dataset with.
+          Name of the raw data
+        institution : Optional[Institution]
+          Primary Institution. Defaults to AIND.
+        funding_source : Optional[Tuple]
+          Tuple of funding sources. Defaults to (AIND)
 
         """
         funding_source_list = (
@@ -116,110 +342,4 @@ class RawDataDescriptionMetadata:
             institution=institution,
             funding_source=funding_source_list,
         )
-        return data_description_instance
-
-
-class ProceduresMetadata:
-
-    def __init__(self, procedures_info: Optional[dict] = None):
-        self.procedures_info = procedures_info
-
-    @property
-    def output_filename(self):
-        return Procedures.construct().default_filename()
-
-    @classmethod
-    def from_file(cls, file_location: Path):
-        with open(file_location) as f:
-            contents = json.load(f)
-        return cls(procedures_info=contents)
-
-    @classmethod
-    def from_service(cls, subject_id, domain):
-        ams_client = AindMetadataServiceClient(domain=domain)
-        response = ams_client.get_procedures(subject_id=subject_id)
-        response_json = response.json()
-        status_code = response.status_code
-        # Everything is okay
-        if status_code == 200:
-            contents = response_json["data"]
-        # Multiple items were found
-        elif status_code == 300:
-            logging.warning(response_json["message"])
-            contents = response_json["data"][0]
-        # The data retrieved is invalid
-        elif status_code == 406:
-            logging.warning(response_json["message"])
-            contents = response_json["data"]
-        # Connected to the service, but no data was found
-        elif status_code == 404:
-            logging.warning(response_json["message"])
-            contents = response_json["data"]
-        else:
-            logging.error(response_json["message"])
-            contents = json.loads(Procedures.construct().json())
-        return cls(procedures_info=contents)
-
-    def validate_obj(self) -> bool:
-        *_, validation_error = (
-            pydantic.validate_model(Procedures, self.procedures_info)
-        )
-        if validation_error:
-            logging.warning(f"Validation Errors: {validation_error}")
-            return False
-        else:
-            logging.info("Procedures model is valid.")
-            return True
-
-
-class SubjectMetadata:
-
-    def __init__(self, subject_info: Optional[dict] = None):
-        self.subject_info = subject_info
-
-    @property
-    def output_filename(self):
-        return Subject.construct().default_filename()
-
-    @classmethod
-    def from_file(cls, file_location: Path):
-        with open(file_location) as f:
-            contents = json.load(f)
-        return cls(subject_info=contents)
-
-    @classmethod
-    def from_service(cls, subject_id, domain):
-        ams_client = AindMetadataServiceClient(domain=domain)
-        response = ams_client.get_subject(subject_id=subject_id)
-        response_json = response.json()
-        status_code = response.status_code
-        # Everything is okay
-        if status_code == 200:
-            contents = response_json["data"]
-        # Multiple items were found
-        elif status_code == 300:
-            logging.warning(response_json["message"])
-            contents = response_json["data"][0]
-        # The data retrieved is invalid
-        elif status_code == 406:
-            logging.warning(response_json["message"])
-            contents = response_json["data"]
-        # Connected to the service, but no data was found
-        elif status_code == 404:
-            logging.warning(response_json["message"])
-            contents = response_json["data"]
-        else:
-            logging.error(response_json["message"])
-            contents = json.loads(Subject.construct().json())
-        return cls(subject_info=contents)
-
-    def validate_obj(self) -> bool:
-        *_, validation_error = (
-            pydantic.validate_model(Subject, self.subject_info)
-        )
-        if validation_error:
-            logging.warning(f"Validation Errors: {validation_error}")
-            return False
-        else:
-            logging.info("Subject model is valid.")
-            return True
+        return cls(model_obj=data_description_instance.dict())
