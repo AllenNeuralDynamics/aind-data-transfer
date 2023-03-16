3ca52dbb41ab8db6d7a6f24045eabdb9d8200dd4
Alessio Buccino <alejoe9187@gmail.com>
2023-03-16 12:42:41 +0100

    fix: remove _get_median_and_lsb and propagate args

--- src/aind_data_transfer/transformations/ephys_compressors.py
+++ src/aind_data_transfer/transformations/ephys_compressors.py
@@ -46,68 +46,6 @@ class EphysCompressors:
             )
 
     @staticmethod
-    def _get_median_and_lsb(
-        recording,
-        disable_tqdm=False,
-        **random_chunk_kwargs,
-    ):
-        """This function estimates the channel-wise medians and the overall
-        LSB from a recording
-        Parameters
-        ----------
-        recording : si.BaseRecording
-            The input recording object
-        disable_tqdm : bool, optional
-            Disable progress bar, default is False
-        **random_chunk_kwargs: keyword arguments for
-            si.get_random_data_chunks() (chunk_size, num_chunks_per_segment)
-        Returns
-        -------
-        int
-            lsb_value
-        np.array
-            median_values
-        """
-        # gather chunks
-        chunks = si.get_random_data_chunks(
-            recording, seed=0, **random_chunk_kwargs
-        )
-
-        lsb_value = 0
-        num_channels = recording.get_num_channels()
-        dtype = recording.get_dtype()
-
-        channel_idxs = np.arange(num_channels)
-        min_values = np.zeros(num_channels, dtype=dtype)
-        median_values = np.zeros(num_channels, dtype=dtype)
-        offsets = np.zeros(num_channels, dtype=dtype)
-
-        for ch in tqdm(
-            channel_idxs, desc="Estimating channel stats", disable=disable_tqdm
-        ):
-            unique_vals = np.unique(chunks[:, ch])
-            unique_vals_abs = np.abs(unique_vals)
-            # it might happen that there is a single unique value
-            # (e.g. a channel is broken)
-            if len(unique_vals) > 1:
-                lsb_val = np.min(np.diff(unique_vals))
-            else:
-                # in this case we can't estimate the LSB for the channel
-                lsb_val = 0
-
-            min_values[ch] = np.min(unique_vals_abs)
-            median_values[ch] = np.median(chunks[:, ch]).astype(dtype)
-
-            unique_vals_m = np.unique(chunks[:, ch] - median_values[ch])
-            unique_vals_abs_m = np.abs(unique_vals_m)
-            offsets[ch] = np.min(unique_vals_abs_m)
-
-            if lsb_val > lsb_value:
-                lsb_value = lsb_val
-
-        return lsb_value, median_values
-
-    @staticmethod
     def scale_read_blocks(
         read_blocks,
         num_chunks_per_segment=100,
@@ -137,7 +75,9 @@ class EphysCompressors:
             ):
                 rec_to_compress = read_block["recording"]
             else:
-                rec_to_compress = spre.correct_lsb(read_block["recording"])
+                rec_to_compress = spre.correct_lsb(read_block["recording"], 
+                                                   num_chunks_per_segment=num_chunks_per_segment,
+                                                   chunk_size=chunk_size)
             yield (
                 {
                     "scaled_recording": rec_to_compress,
