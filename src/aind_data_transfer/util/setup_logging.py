9ba72848f7e8d9b9308a0eb7c46be8ebd7f4739d
Cameron Arshadi <arshadic23@gmail.com>
2023-09-05 14:43:33 -0700

    use custom filter instead of LogRecord

--- src/aind_data_transfer/util/setup_logging.py
+++ src/aind_data_transfer/util/setup_logging.py
@@ -1,23 +1,35 @@
 import logging
-import logging.config
 import socket
 
 
-class HostnameLogRecord(logging.LogRecord):
-    def getMessage(self):
-        msg = super().getMessage()
-        return f"[{socket.gethostname()}] {msg}"
+class HostnameFilter(logging.Filter):
+    hostname = socket.gethostname()
 
+    def filter(self, record):
+        record.hostname = self.hostname
+        return True
 
-logging.setLogRecordFactory(HostnameLogRecord)
 
 logging_config = {
     'version': 1,
     'disable_existing_loggers': False,
+    'filters': {
+        'add_hostname': {
+            '()': HostnameFilter,
+        },
+    },
+    'formatters': {
+        'detailed': {
+            'format': '%(asctime)s [%(hostname)s] %(name)s: %(message)s',
+            'datefmt': '%Y-%m-%d %H:%M:%S',
+        },
+    },
     'handlers': {
         'console': {
             'class': 'logging.StreamHandler',
             'stream': 'ext://sys.stdout',
+            'formatter': 'detailed',
+            'filters': ['add_hostname'],
         },
     },
     'loggers': {
