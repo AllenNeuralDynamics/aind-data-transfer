57d2a4d8f78fd0c333a180f8cbeb6050ebd5be6f
Cameron Arshadi <arshadic23@gmail.com>
2023-10-02 15:42:48 -0700

    feat: updates test_flatfield_correction.py

--- tests/test_flatfield_correction.py
+++ tests/test_flatfield_correction.py
@@ -10,12 +10,30 @@ from aind_data_transfer.transformations.flatfield_correction import BkgSubtracti
 class TestBkgSubtraction(unittest.TestCase):
 
     def test_subtract(self):
-        im = da.from_array(np.full((64, 64, 64), fill_value=100, dtype=int), chunks=(16, 16, 16))
-        bkg_im = da.from_array(np.full((32, 32), fill_value=50, dtype=int), chunks=(16, 16))
+        im = da.from_array(np.full((64, 64, 64), fill_value=100, dtype=np.uint16), chunks=(16, 16, 16))
+        bkg_im = da.from_array(np.full((32, 32), fill_value=50, dtype=np.uint16), chunks=(16, 16))
         result = BkgSubtraction.subtract(im, bkg_im)
         self.assertIsInstance(result, da.Array)
         self.assertEqual(result.shape, im.shape)
+        self.assertEqual(result.dtype, np.uint16)
         self.assertEqual(result.sum().compute(), np.product(im.shape) * 50)
+
+        # test when bkg_im is larger than im
+        bkg_im = da.from_array(np.full((128, 128), fill_value=50, dtype=np.uint16), chunks=(16, 16))
+        result = BkgSubtraction.subtract(im, bkg_im)
+        self.assertIsInstance(result, da.Array)
+        self.assertEqual(result.shape, im.shape)
+        self.assertEqual(result.dtype, np.uint16)
+        self.assertEqual(result.sum().compute(), np.product(im.shape) * 50)
+
+        # test clipping behavior
+        im = da.from_array(np.full((64, 64, 64), fill_value=100, dtype=np.uint16), chunks=(16, 16, 16))
+        bkg_im = da.from_array(np.full((64, 64), fill_value=200, dtype=np.uint16), chunks=(16, 16))
+        result = BkgSubtraction.subtract(im, bkg_im)
+        self.assertIsInstance(result, da.Array)
+        self.assertEqual(result.shape, im.shape)
+        self.assertEqual(result.dtype, np.uint16)
+        self.assertEqual(result.sum().compute(), 0)
 
     def test_get_bkg_path(self):
         bkg_im_dir = "path/to/background_images/"
