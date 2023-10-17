2692006dbaac0858aabe664f72320d39c3058220
Mekhla Kapoor <54870020+mekhlakapoor@users.noreply.github.com>
2023-10-17 14:46:24 -0700

    metadata record (#292)
    * metadata record
    
    * unit tests
    
    * removes args
    
    * last_modified=created

--- src/aind_data_transfer/jobs/basic_job.py
+++ src/aind_data_transfer/jobs/basic_job.py
@@ -7,6 +7,7 @@ import os
 import shutil
 import sys
 import tempfile
+import uuid
 from datetime import datetime, timezone
 from enum import Enum
 from importlib import import_module
@@ -27,6 +28,7 @@ from aind_data_transfer.transformations.metadata_creation import (
     ProcessingMetadata,
     RawDataDescriptionMetadata,
     SubjectMetadata,
+    MetadataRecord
 )
 from aind_data_transfer.util.s3_utils import upload_to_s3
 
@@ -241,6 +243,24 @@ class BasicJob:
         )
         processing_file_name = temp_dir / processing_metadata.output_filename
         processing_metadata.write_to_json(processing_file_name)
+
+        created = datetime.utcnow()
+        last_modified = created
+        data_asset_id = str(uuid.uuid4())
+
+        metadata = MetadataRecord.from_inputs(
+            id=data_asset_id,
+            name=self.job_configs.s3_prefix,
+            created=created,
+            last_modified=last_modified,
+            location=self.job_configs.s3_bucket,
+            subject_metadata=subject_metadata,
+            procedures_metadata=procedures_metadata,
+            processing_metadata=processing_metadata,
+            data_description_metadata=data_description_metadata
+        )
+        metadata_file_name = temp_dir / metadata.output_filename
+        metadata.write_to_json(metadata_file_name)
 
         # Check user defined metadata
         if self.job_configs.metadata_dir:
