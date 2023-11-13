1276b84f9d0404ed5ebb081498a293644647e5d1
Carson Berry <carson.berry@carsonb-ux1.corp.alleninstitute.org>
2023-11-13 14:54:32 -0800

    fixing typos in configs and sbatch cron job

--- scripts/bin/HCR_chron_job.sh
+++ scripts/bin/HCR_chron_job.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-#SBATCH -N1 # Run this across 1 node
-#SBATCH -c3 # Request cpus
+
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2# Request cpus
 #SBATCH --mem=10gb
 #SBATCH --partition aind
 #SBATCH --exclude=n69,n74
@@ -11,11 +12,11 @@
 #SBATCH --mail-user=carson.berry@alleninstitute.org
 
 set -e
-
+echo "Starting"
 pwd; date
 [[ -f "/allen/programs/mindscope/workgroups/omfish/carsonb/miniconda/bin/activate" ]] && source "/allen/programs/mindscope/workgroups/omfish/carsonb/miniconda/bin/activate" adt-upload-clone
 
-python /allen/aind/scratch/carson.berry/aind_data_transfer/scripts/ispim_chron_job.py --config_file /allen/aind/scratch/carson.berry/aind-data-transfer/scritps/chron_job_configs.yml
+python /allen/aind/scratch/carson.berry/aind-data-transfer/scripts/ispim_cron_job.py --config_file /allen/aind/scratch/carson.berry/aind-data-transfer/scripts/chron_job_configs.yml
 
 echo "Done"
 date
