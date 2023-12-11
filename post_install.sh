c68fc944b72bec05f1e67c627bc518cac476e6ec
arjun.sridhar12345 <arjun.sridhar1998@gmail.com>
2023-12-11 12:41:36 -0800

    build update probe interface package

--- post_install.sh
+++ post_install.sh
@@ -3,4 +3,3 @@
 python -m pip install "git+https://github.com/AllenNeuralDynamics/aind-ng-link@feat-zarr-checker#egg=aind-ng-link" --no-cache-dir
 python -m pip install "git+https://github.com/fsspec/kerchunk" --no-cache-dir
 python -m pip install hdf5plugin --no-binary hdf5plugin --no-cache-dir
-python -m pip install "git+https://github.com/arjunsridhar12345/python-neo.git" --no-cache-dir
