8b42ff7e114fe956770c5675fa5da5c2a1bd7f73
Cameron Arshadi <arshadic23@gmail.com>
2024-01-22 15:33:25 -0800

    Feat: pull aind-ng-link from pypi

--- post_install.sh
+++ post_install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-python -m pip install "git+https://github.com/AllenNeuralDynamics/aind-ng-link" --no-cache-dir
 python -m pip install "git+https://github.com/fsspec/kerchunk" --no-cache-dir
 python -m pip install hdf5plugin --no-binary hdf5plugin --no-cache-dir
